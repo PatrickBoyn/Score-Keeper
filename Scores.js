87736939ec6fa53ada25922fd4200e42fa4480c9
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-08 18:17:38 -0500

    Added event listeners to the first two buttons. Made them an alert for easy checking.

--- Scores.js
+++ Scores.js
@@ -1,2 +1,12 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.querySelector("#p2");
+
+p1Button.addEventListener("click", function(){
+    // for testing purposes. 
+    alert("I was clicked!");
+});
+
+p2Button.addEventListener("click", function(){
+    // for testing purposes. 
+    alert("I was clicked!");
+});

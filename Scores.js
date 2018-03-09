575b290d0372799190c3fa358582f302df25a0a3
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-08 22:43:35 -0500

    Added the first bit of game over logic.

--- Scores.js
+++ Scores.js
@@ -7,15 +7,21 @@ var p2Display = document.getElementById("p2Display");
 // Counters
 var p1Score = 0;
 var p2Score = 0;
+var winningScore = 7;
 
 // Checks the state of the game
 var gameOver = false;
 
 // Makes the score go up when the button is pressed
 p1Button.addEventListener("click", function(){
-    p1Score++;
-    p1Display.textContent = p1Score;
-    console.log(p1Score);
+    if(!gameOver){
+        p1Score++;
+        if(p1Score === winningScore){
+            console.log("Game Over!");
+        }
+        p1Display.textContent = p1Score;
+        console.log(p1Score);
+    }
 });
 
 p2Button.addEventListener("click", function(){

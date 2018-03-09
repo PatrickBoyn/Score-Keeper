b520e01debd5bcf7a22e4dc464d64c5a4ac08e34
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-08 21:37:41 -0500

    There was one change. Can you guess what it was? A typo.

--- Scores.js
+++ Scores.js
@@ -1,9 +1,9 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.querySelector("#p2");
-var p1Sscore = 0;
+var p1Score = 0;
 
 
 p1Button.addEventListener("click", function(){
-    // for testing purposes. 
-    alert("I was clicked!");
+    p1Score++;
+    console.log(p1Score);
 });

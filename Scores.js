f98e95b325396f092f87f40dcf4646aa554f29d3
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-08 22:28:08 -0500

    Added comments about the code.

--- Scores.js
+++ Scores.js
@@ -1,10 +1,14 @@
+// Selectors
 var p1Button = document.getElementById("p1");
 var p2Button = document.getElementById("p2");
 var p1Display = document.getElementById("p1Display");
 var p2Display = document.getElementById("p2Display");
+
+// Counters
 var p1Score = 0;
 var p2Score = 0;
 
+// Makes the score go up when the button is pressed
 p1Button.addEventListener("click", function(){
     p1Score++;
     p1Display.textContent = p1Score;
@@ -16,4 +20,3 @@ p2Button.addEventListener("click", function(){
     p2Display.textContent = p2Score;
     console.log(p2Score);
 });
-

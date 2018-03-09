d45eb9f6bce0063b2159d2131147d7f316cda0eb
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-08 21:50:38 -0500

    Changed the selector from h1 to the span id for player one. Also connected it to the html.

--- Scores.js
+++ Scores.js
@@ -1,11 +1,11 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.querySelector("#p2");
-var h1 = document.querySelector("h1");
+var p1Display = document.getElementById("p1Display");
 var p1Score = 0;
 
 
 p1Button.addEventListener("click", function(){
     p1Score++;
-    h1.textContent = p1Score;
+    p1Display.textContent = p1Score;
     console.log(p1Score);
 });

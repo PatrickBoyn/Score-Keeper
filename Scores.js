f146cdf1b6ecf7bfd815c0442d3a7d71007a72b9
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-08 21:35:27 -0500

    Deleted the non functiong alert maker. Added a counter.

--- Scores.js
+++ Scores.js
@@ -1,11 +1,9 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.querySelector("#p2");
+var p1Sscore = 0;
+
 
 p1Button.addEventListener("click", function(){
     // for testing purposes. 
     alert("I was clicked!");
-});
-
-p2button.addEventListener("click", function(){
-    alert("I was clicked");
 });
